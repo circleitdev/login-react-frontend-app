6d1730f1afedfe28e95894a4a932e009cab4d98c
bforbilly24 <halimputra0701@gmail.com>
2024-05-07 17:59:02 +0700

    feat(components): add /components/miscellaneous/not-ready-responsive

---
+++ src/components/miscellaneous/not-ready-responsive.jsx
@@ -0,0 +1,10 @@
+const NotReadyResponsive = () => {
+	return (
+		<div className='flex h-screen flex-col place-content-center items-center justify-center p-5 text-justify xl:hidden'>
+			<div>[WARN]</div>
+			<div>Please use desktop device instead of using smartphone, tablet or small desktop for best user experience purpose only !</div>
+		</div>
+	);
+};
+
+export default NotReadyResponsive;
