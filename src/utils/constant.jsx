7bbc576fa7cb22765a5861ad6dedfdb1f5b1959e
bforbilly24 <halimputra0701@gmail.com>
2024-05-07 17:57:50 +0700

    feat(page): add /utils/constant

---
+++ src/utils/constant.jsx
@@ -0,0 +1,34 @@
+const images = [
+	{
+		path: '/brand.svg',
+		alt: 'Brand Image',
+		srcset: '/brand.svg 1x, /brand@2x.svg 2x',
+	},
+	{
+		path: '/show.svg',
+		alt: 'Show',
+		srcset: '/show.svg 1x, /show@2x.svg 2x',
+	},
+	{
+		path: '/hide.svg',
+		alt: 'Hide',
+		srcset: '/hide.svg 1x, /hide@2x.svg 2x',
+	},
+];
+
+const users = [
+	{
+		email: 'belly@circleit.dev',
+		password: 'belly123',
+	},
+	{
+		email: 'supre@circleit.dev',
+		password: 'supre123',
+	},
+	{
+		email: 'bforbilly@circleit.dev',
+		password: 'bforbilly24',
+	},
+];
+
+export { images, users };
